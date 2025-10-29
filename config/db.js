f85a07e1e2f332ab28a39e5145b89b7008d92dfc
ayodoubleayo <ayodoubleayo@gmail.com>
2025-10-29 09:35:33 -0800

    Prepare for Render deployment

--- config/db.js
+++ config/db.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/restapi_portfolio");
-    console.log("✅ MongoDB Connected Successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
     process.exit(1);
